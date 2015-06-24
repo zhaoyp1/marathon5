c4ecfe73d484d1cd52a3cd01d4ae3293f73a013b
liuwei29 <liuwei29@asiainfo.com>
2015-06-24 14:51:43 +0800

    dateUtils

--- src/main/java/com/asiainfo/baas/common/DateUtils.java
+++ src/main/java/com/asiainfo/baas/common/DateUtils.java
@@ -2,6 +2,8 @@ package com.asiainfo.baas.common;
 
 import java.util.Date;
 
+import com.asiainfo.baas.marathon.baseType.TimePeriod;
+
 public class DateUtils {
 
     /**
@@ -14,6 +16,26 @@ public class DateUtils {
      */
     public static int compareDate(Date d1, Date d2) {
         return d1.compareTo(d2);
+    }
+
+    /**
+     * 校验时间点是否在TimePeriod内
+     * 
+     * 
+     * 
+     * @param d1
+     * @param d2
+     */
+    public static boolean isInPeriod(Date targetDate, TimePeriod validFor) {
+
+        // 如果时间段未设置，默认永久有效
+        if (validFor.startDateTime != null && compareDate(targetDate, validFor.startDateTime) < 0) {
+            return false;
+        }
+        if (validFor.endDateTime != null && compareDate(targetDate, validFor.endDateTime) > 0) {
+            return false;
+        }
+        return true;
     }
 
 }
