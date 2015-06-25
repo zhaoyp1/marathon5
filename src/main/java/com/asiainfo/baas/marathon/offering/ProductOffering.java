25d58e73453ec45dbacae4f83fe56e57f1e5762e
liuwei29 <liuwei29@asiainfo.com>
2015-06-25 15:55:32 +0800

    update

--- src/main/java/com/asiainfo/baas/marathon/offering/ProductOffering.java
+++ src/main/java/com/asiainfo/baas/marathon/offering/ProductOffering.java
@@ -166,11 +166,11 @@ public abstract class ProductOffering {
      */
     public ProductOffering[] queryRelatedOffering(String relationType, Date time) {
     	List<ProductOffering> offering = null;
-    	if(prodOfferingRelationship != null && prodOfferingRelationship.size()>0){
+    	if(prodOfferingRelationship == null && prodOfferingRelationship.size()>0){
     		offering = new ArrayList<ProductOffering>();
     		for(int i = 0 ; i < prodOfferingRelationship.size() ; i++){
     			ProductOfferingRelationship relatedOffering = prodOfferingRelationship.get(i);
-    			if(relationType.equals(relatedOffering.getTypeRelationship()) && DateUtils.isInPeriod(time, relatedOffering.getValidFor())){
+    			if(relationType.equals(relatedOffering.getTypeRelationship()) && relatedOffering.getValidFor().isInPeriod(time)){
     				offering.add(relatedOffering.getTargetOffering());
     			}
     		}
