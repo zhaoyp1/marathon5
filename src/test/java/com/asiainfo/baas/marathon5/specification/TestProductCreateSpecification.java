b474dbd655bcb0c0caa873196deb8adf0b4d3cf8
Zhao YunPing <zhaoyp@asiainfo.com>
2015-06-25 20:00:42 +0800

    3

--- src/test/java/com/asiainfo/baas/marathon5/specification/TestProductCreateSpecification.java
+++ src/test/java/com/asiainfo/baas/marathon5/specification/TestProductCreateSpecification.java
@@ -1,14 +1,27 @@
 package com.asiainfo.baas.marathon5.specification;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import net.sf.json.JSONObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.asiainfo.baas.marathon.baseType.Money;
 import com.asiainfo.baas.marathon.baseType.TimePeriod;
+import com.asiainfo.baas.marathon.specification.AtomicProductSpecification;
+import com.asiainfo.baas.marathon.specification.ProdSpecCharValueUse;
+import com.asiainfo.baas.marathon.specification.ProductSpecCharUse;
 import com.asiainfo.baas.marathon.specification.ProductSpecCharacteristic;
 import com.asiainfo.baas.marathon.specification.ProductSpecCharacteristicValue;
+import com.asiainfo.baas.marathon.specification.ProductSpecification;
 
 public class TestProductCreateSpecification {
 
-	List<ProductSpecCharacteristic> productSpecChars;
+	static List<ProductSpecCharacteristic> productSpecChars;
+	@Before
 	public void createProductSpecChar(){
 		TimePeriod validFor = new TimePeriod("2015-02-03 12:00:00","2015-07-21 23:59:59");
 		ProductSpecCharacteristic productSpecCharProcessor=new ProductSpecCharacteristic("1","processor(处理器)", "number",validFor , "unique",1,1);
@@ -34,7 +47,94 @@ public class TestProductCreateSpecification {
 		productSpecCharSizeAndWeight.addLeafCharacteristic(productSpecCharWidth, validFor);
 		productSpecCharSizeAndWeight.addLeafCharacteristic(productSpecCharDepth, validFor);
 		productSpecCharSizeAndWeight.addLeafCharacteristic(productSpecCharWeight, validFor);
+		productSpecChars=new ArrayList<ProductSpecCharacteristic>();
 		productSpecChars.add(productSpecCharSizeAndWeight);
+		productSpecChars.add(productSpecCharHeight);
+		productSpecChars.add(productSpecCharWidth);
+		productSpecChars.add(productSpecCharDepth);
+		productSpecChars.add(productSpecCharWeight);
 		productSpecChars.add(productSpecCharProcessor);
 	}
+	
+	@Test
+	public void createProductSpecification() throws Exception{
+		
+		TimePeriod validFor = new TimePeriod("2015-02-03 12:00:00","2015-07-21 23:59:59");
+		ProductSpecification productSpec=new AtomicProductSpecification("mac-13","13-inch MacBook Pro","apple","in_active");
+		ProductSpecCharacteristic prodSpecChar=this.getCharByCharName("size and weight");
+		productSpec.addCharacteristic(prodSpecChar, true, true, validFor);
+		prodSpecChar=this.getCharByCharName("Height");
+		productSpec.addCharacteristic(prodSpecChar, true, true, validFor);
+		ProductSpecCharacteristicValue[] values=this.getCharValue(prodSpecChar,new int[]{0});
+		if(values!=null){
+			for (ProductSpecCharacteristicValue productSpecCharacteristicValue : values) {
+				productSpec.attachCharacteristicValue(prodSpecChar,productSpecCharacteristicValue, true, validFor);
+			}
+		}
+		//添加成本
+		Money cost=new Money("min",109);
+		productSpec.addCost(cost, validFor);
+		productSpec.setVersion("1.0.0", "",new Date(), validFor);
+		 
+//		System.out.println(JSONObject.fromObject(productSpec).toString());
+		System.out.print("规格信息:");
+		System.out.println("name:"+productSpec.getName()+"  ,   brand:"+productSpec.getProductNumber()+""+productSpec.getBrand());
+		
+		List<ProductSpecCharUse> productSpecCharUses=productSpec.getProdSpecCharUse();
+		for (ProductSpecCharUse productSpecCharUse : productSpecCharUses) {
+			ProductSpecCharacteristic useprodSpecChar=productSpecCharUse.getProdSpecChar();
+			System.out.println("特征：");
+			System.out.println("name:"+useprodSpecChar.getName()+"  ,   valueType:"+useprodSpecChar.getValueType());
+			List<ProdSpecCharValueUse> valueUses=productSpecCharUse.getProdSpecCharValueUse();
+			if(valueUses!=null){
+				for (ProdSpecCharValueUse prodSpecCharValueUse : valueUses) {
+					ProductSpecCharacteristicValue charValue=prodSpecCharValueUse.getProdSpecCharValue();
+					System.out.println("特征值：");	
+
+					if(charValue.getValue()!=null){
+						System.out.println(" value:"+charValue.getValueFrom());	
+
+					}else{
+						System.out.println(" valueFrom:"+charValue.getValueFrom()+" ,   valueTo: "+charValue.getValueTo());	
+					}
+					
+				}
+			}
+			
+		}
+		
+	}
+	
+	public ProductSpecCharacteristic getCharByCharName(String name){
+		ProductSpecCharacteristic prodSpecChar=null;
+		for (int i = 0; i < productSpecChars.size(); i++) {
+			prodSpecChar=productSpecChars.get(i);
+			if(name.equals(prodSpecChar.getName())){
+				return prodSpecChar;
+			}
+		}
+		return null;
+		 
+		
+	}
+	
+	public ProductSpecCharacteristicValue[] getCharValue(ProductSpecCharacteristic characteristic,int[] ids){
+		if(ids!=null){
+			 List<ProductSpecCharacteristicValue> productValues=characteristic.getProductSpecCharacteristicValue();
+			 List<ProductSpecCharacteristicValue> prodSpecChars=new ArrayList<ProductSpecCharacteristicValue>();
+			 
+			 for (int id  : ids) {
+				for (int i=0 ; i<productValues.size() ; i++) {
+					if(id==i){
+						prodSpecChars.add(productValues.get(i));
+						break;
+					}
+				}
+			 }	
+			 return (ProductSpecCharacteristicValue[])prodSpecChars.toArray(new ProductSpecCharacteristicValue[prodSpecChars.size()]);
+		}
+		return null;
+		
+	}
+	
 }
