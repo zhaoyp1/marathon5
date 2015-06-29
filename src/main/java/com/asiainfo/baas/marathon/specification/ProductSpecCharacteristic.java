c86245cb2c532a44b25ce8168beb1de95a84e091
liurl3 <liurl3@asiainfo.com>
2015-06-29 20:06:48 +0800

    equals方法的测试

--- src/main/java/com/asiainfo/baas/marathon/specification/ProductSpecCharacteristic.java
+++ src/main/java/com/asiainfo/baas/marathon/specification/ProductSpecCharacteristic.java
@@ -426,8 +426,12 @@ public class ProductSpecCharacteristic {
 		if (validFor == null) {
 			if (other.validFor != null)
 				return false;
-		} else if (!validFor.equals(other.validFor))
-			return false;
+		} else {
+			if (!validFor.getStartDateTime().equals(other.validFor.getStartDateTime()))
+				return false;
+			if (!validFor.getEndDateTime().equals(other.validFor.getEndDateTime()))
+				return false;
+		} 
 		if (valueType == null) {
 			if (other.valueType != null)
 				return false;
